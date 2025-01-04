b6b0e8a933554b8a134f661272b13aa3cae222a9
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2025-01-04 12:47:57 +0100

    day 24.2 solved

--- day_24.py
+++ day_24.py
@@ -1,14 +1,18 @@
-with open ("data/day_24.txt") as f:
-    nodes = dict()
-    rules = set()
-    for line in f:
-        if ':' in line:
-            k, v = line.strip().split(': ')
-            nodes[k] = int(v)
-        elif ' -> ' in line:
-            r, target = line.strip().split(' -> ')
-            a, op, b = r.split(' ')
-            rules.add((a, b, op, target))
+def read_input():
+    with open ("data/day_24.txt") as f:
+        nodes = dict()
+        rules = set()
+        for line in f:
+            if ':' in line:
+                k, v = line.strip().split(': ')
+                nodes[k] = int(v)
+            elif ' -> ' in line:
+                r, target = line.strip().split(' -> ')
+                a, op, b = r.split(' ')
+                rules.add((a, b, op, target))
+    return rules, nodes
+
+rules, nodes = read_input()
 
 while rules:
     r = rules.pop()
@@ -23,9 +27,24 @@ while rules:
     else:
         rules.add(r)
 
+
 zs = {k: v for k, v in nodes.items() if k.startswith('z')}
 z_number = ''
 for i in range(len(zs)):
     z_number = str(zs[f'z{i:02}']) + z_number
-print(z_number)
 print(int(z_number, 2))
+
+rules, nodes = read_input()
+
+import networkx as nx
+import gravis as gv
+import matplotlib.pyplot as plt
+G = nx.DiGraph()
+for rule in rules:
+    clr = 'red' if rule[2] == 'XOR' else 'black'
+    G.add_edge(rule[0], rule[3], color=clr)
+    G.add_edge(rule[1], rule[3], color=clr)
+
+gv.d3(G)
+
+print(','.join(sorted(['qqp', 'z23', 'pbv', 'z16', 'qff', 'qnw', 'fbq', 'z36'])))
