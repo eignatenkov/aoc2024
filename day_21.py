8d657487f17fee4ad925b551407c934fbecb15d7
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2025-01-04 18:43:20 +0100

    21.2 at least finishes but not good enough

--- day_21.py
+++ day_21.py
@@ -1,12 +1,12 @@
 import numpy as np
-from itertools import chain
-from functools import lru_cache
-from tqdm import tqdm
+from collections import Counter
+
+BEST_MAP = dict()
+
 
 def manhattan_distance(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-@lru_cache(maxsize=1000000)
 def find_all_instructions(code, cur_pos='A', numeric=True):
     if numeric:
         keyboard = np.array([['7','8','9'],['4','5','6'], ['1','2','3'], ['#', '0', 'A']], dtype=str)
@@ -42,42 +42,53 @@ def find_all_instructions(code, cur_pos='A', numeric=True):
         return paths
 
 
+def instruction_to_dict(instruction):
+    return Counter(j + 'A' for j in instruction[:-1].split('A'))
+
+def len_dict_repr(dict_repr):
+    return sum(len(k)*v for k, v in dict_repr.items())
+
+
+def unroll_dict(instruction_dict):
+    global BEST_MAP
+    new_dict = Counter()
+    for k, v in instruction_dict.items():
+        if k not in BEST_MAP:
+            BEST_MAP[k] = find_best_repr(k)
+        for sub_words, n in BEST_MAP[k].items():
+            new_dict[sub_words] += n * v
+    return new_dict
+
+
+def find_best_repr(subword):
+    first_level = find_all_instructions(subword, numeric=False)
+    best_repr = None
+    min_len = float('inf')
+    for fl in first_level:
+        sl = find_all_instructions(fl, numeric=False)
+        for s in sl:
+            if len(s) <= min_len:
+                min_len = len(s)
+                best_repr = fl
+    return instruction_to_dict(best_repr)
+
+#287046661958274
 def shortest_instruction(code, n_directional_iterations=2):
-    cur_it = find_all_instructions(code, numeric=True)
-    for _ in tqdm(range(n_directional_iterations)):
-        cur_it = list(chain.from_iterable(find_all_instructions(c, numeric=False) for c in cur_it))
-        min_instr = min(len(s) for s in cur_it)
-        cur_it = [c for c in cur_it if len(c) == min_instr]
-    return min(len(c) for c in cur_it)
+    global BEST_MAP
+    first_reps = find_all_instructions(code, numeric=True)
+    best_len = float('inf')
+    for fr in first_reps:
+        rep_dict = instruction_to_dict(fr)
+        for i in range(n_directional_iterations):
+            rep_dict = unroll_dict(rep_dict)
+        len_rep = len_dict_repr(rep_dict)
+        if len_rep < best_len:
+            best_len = len_rep
+    return best_len
 
-
-def type_code(code, numeric=True):
-    if numeric:
-        keyboard = np.array([['7','8','9'],['4','5','6'], ['1','2','3'], ['#', '0', 'A']], dtype=str)
-    else:
-        keyboard = np.array([['#', '^', 'A'], ['<', 'v', '>']], dtype=str)
-    cur_pos = 'A'
-    instructions = []
-    for button in list(code):
-        cur_x, cur_y = np.argwhere(keyboard == cur_pos)[0]
-        new_x, new_y = np.argwhere(keyboard == button)[0]
-        if new_y > cur_y:
-            instructions += ['>'] * (new_y - cur_y)
-            if new_x < cur_x:
-                instructions += ['^'] * (cur_x - new_x)
-            else:
-                instructions += ['v'] * (new_x - cur_x)
-        else:
-            if new_x < cur_x:
-                instructions += ['^'] * (cur_x - new_x)
-            else:
-                instructions += ['v'] * (new_x - cur_x)
-            instructions += ['<'] * (cur_y - new_y)
-        instructions.append('A')
-        cur_pos = button
-    return ''.join(instructions)
 
 # test_codes = ['029A', '980A', '179A', '456A', '379A']
 test_codes = ['480A', '143A', '983A', '382A', '974A']
-print(sum(int(code[:3]) * shortest_instruction(code) for code in test_codes))
-print(sum(int(code[:3]) * shortest_instruction(code, 5) for code in test_codes))
+print(sum(int(code[:3]) * shortest_instruction(code, 2) for code in test_codes))
+print(sum(int(code[:3]) * shortest_instruction(code, 25) for code in test_codes))
+print(BEST_MAP)
